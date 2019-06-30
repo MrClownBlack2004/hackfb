13c901546365154f8ec83f546fbf5037b1fcffbf
MrClownBlack2004 <52379236+MrClownBlack2004@users.noreply.github.com>
2019-06-30 22:21:03 +0700

    jhon.sh
    >:(

---
+++ jhon.sh
@@ -0,0 +1,33 @@
+clear
+figlet Welcome | lolcat
+echo "==============================
+
+Nama  : MrClownBlack
+IG    : Jhon_Manikz06
+Fb    : JHON CHOLOSIAN MANIKZ
+YT    : JHON MANIKZ
+
+===============================" | lolcat
+
+echo "
+[1]Hack Facebook Massal(Mbf)
+[2]exit
+pilih Nomor
+[+]" | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+pkg install git
+git clone https://github.com/musjadsakirang/mbf
+ls
+cd mbf
+ls
+python2 MBF.py
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+exit
+fi
