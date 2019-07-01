cba73d5d706a256e41fbbceb299762c0b2bb778b
MrClownBlack2004 <52379236+MrClownBlack2004@users.noreply.github.com>
2019-07-01 09:30:06 +0700

    Hack Fb

---
+++ hack.sh
@@ -0,0 +1,24 @@
+clear
+figlet Welcome | lolcat
+echo "==============================
+
+Name   : Mr.ClownBlack
+Github : MrClownBlack2004
+
+==============================" | lolcat
+
+echo "Kalo Mau Hack Fb Pilih Nomor 1 ya Bgsd
+[1]Hack Fb
+Tulis Nomor 1 di Bawah Goblok" | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+pkg install git
+git clone https://github.com/pashayogi/SETAN
+ls
+cd SETAN
+ls
+python2 SETAN.Py
+fi
